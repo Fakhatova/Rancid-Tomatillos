e28f897b37db767c4e613ca2608a95109dee5ce1
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-25 16:10:37 -0600

    Add nav component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from './Nav/Nav';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
   render() {
     return(
       <main className='App'>
+        <Nav />
         <h1>Rancid Tomatillos</h1>
       </main>
     )
---
+++ src/Nav/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css';
+
+
+const Nav = () => {
+  return (
+    <navbar>
+      <h3>Rancid Tomatillos</h3>
+      <button>Home</button>
+    </navbar>
+  )
+}
+
+export default Nav;
