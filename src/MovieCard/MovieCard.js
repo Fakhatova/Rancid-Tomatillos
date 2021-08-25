5837033171afb00b638002aa489e0c899cf772be
Fara Akhatova <faraakhatova@gmail.com>
2021-08-25 15:03:23 -0600

    Add JSX for movie card component and test h1 element

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 
 const MovieCard = () => {
-
+  return (
+      <main className="movie-card">
+          <h1>HERE IS GONNA BE MOVIE LIST CARDS</h1>
+      </main>
+  )
 }
 
 export default MovieCard
