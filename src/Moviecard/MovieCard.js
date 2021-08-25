2a1347a4cbe49e73bc150112b77beb6a48e6b3ed
Fara Akhatova <faraakhatova@gmail.com>
2021-08-25 17:01:20 -0600

    Add elements for movie crd component

--- src/Moviecard/MovieCard.js
+++ src/Moviecard/MovieCard.js
@@ -1,3 +1,16 @@
 import React from 'react'
+import '../MovieCard/MovieCard.css'
 
-  
+const MovieCard = (props) => {
+  return (
+    <article className="movie-card">
+      <img src="" alt=""/>
+       <h4 className="movie-title"> Title goes here</h4>
+       <p className="movie-ratings"> Ratings goes here</p>
+       <p className="movie-tagline"></p>
+    </article>
+  )
+}
+
+
+  export default MovieCard
