951c4b9697a3b185a760433ef663682284c7c418
Fara Akhatova <faraakhatova@gmail.com>
2021-08-25 17:23:32 -0600

    Finish movieCard component props value being passed and rendered

--- src/Moviecard/MovieCard.js
+++ src/Moviecard/MovieCard.js
@@ -2,13 +2,13 @@ import React from 'react'
 import '../MovieCard/MovieCard.css'
 
 const MovieCard = ({movieCard}) => {
-  console.log(movieCard)
+  // console.log(movieCard)
   return (
     <article className="movie-card">
-      <img  className="movie-image" src="" alt=""/>
+      <img  className="movie-image" src={movieCard.poster_path} alt=""/>
        <h4 className="movie-title"> {movieCard.title}</h4>
-       <p className="movie-ratings"> Ratings goes here</p>
-       <p className="movie-tagline"></p>
+       <p className="movie-ratings"> {movieCard.average_rating}</p>
+       <p className="movie-tagline"> Heres a tagline!</p>
     </article>
   )
 }
