9ebcaab9a6ff75cadb26c59880ab180d3b388dfa
Fara Akhatova <faraakhatova@gmail.com>
2021-08-30 15:02:13 -0600

    Add pseudocode for testing home button on click

--- cypress/integration/mainIndex_spec.js
+++ cypress/integration/mainIndex_spec.js
@@ -9,4 +9,11 @@ it('Should be able visit the page and render Nav bar with correct name and home 
     cy.get('h3').contains('Rancid Tomatillos')
     cy.get('button').contains('Home')
 })
+
+// it('Should be able click a button to get back main page', () => {
+   // we should be able to test button click
+   // what event/type button holds and where it takes us back 
+// })
+
+ 
 })
