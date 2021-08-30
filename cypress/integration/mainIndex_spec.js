dea94ce6d17666b665de1014412be3b35849204d
Fara Akhatova <faraakhatova@gmail.com>
2021-08-30 14:57:26 -0600

    Add get for first it block to test what h3 contains

--- cypress/integration/mainIndex_spec.js
+++ cypress/integration/mainIndex_spec.js
@@ -6,6 +6,7 @@ describe('Main  index user flow', () => {
 
 it('Should be able visit the page and render Nav bar with correct name and home button', () => {
     cy.get('nav')
+    cy.get('h3').contains('Rancid Tomatillos')
     
 })
 })
