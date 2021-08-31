4db4724872a11cdff7c4e2ba8374d107febabf4e
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-31 14:14:08 -0600

    Add movie details component

---
+++ src/MovieDetails/MovieDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../MovieDetails/MovieDetails.css'
+
+const MovieDetails = ({movie}) => {
+  return (
+    <article className="movie-details">
+      <img src={movie.poster_path} alt=""/>
+       <h4> {movie.title}</h4>
+       <p> {movie.average_rating}</p>
+       <p> {movie.tagline}</p>
+    </article>
+  )
+}
+
+
+  export default MovieDetails
