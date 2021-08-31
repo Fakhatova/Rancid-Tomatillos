fbbb5214d28b836f9a6a4b76f9ea06852d1110dd
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-31 14:35:21 -0600

    Add stying for movie details

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -4,10 +4,10 @@ import '../MovieDetails/MovieDetails.css'
 const MovieDetails = ({movie}) => {
   return (
     <article className="movie-details">
-      <img src={movie.poster_path} alt=""/>
-       <h4> {movie.title}</h4>
-       <p> {movie.average_rating}</p>
-       <p> {movie.release_date}</p>
+      <img src={movie.poster_path} alt="" className="movie-image"/>
+       <h4 className="movie-title"> {movie.title}</h4>
+       <p className="movie-ratings"> {movie.average_rating}</p>
+       <p className="movie-release"> {movie.release_date}</p>
     </article>
   )
 }
