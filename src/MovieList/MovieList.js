427a0e33e53a46158dcb00b7701d2ae4ab56a1ce
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-31 13:46:41 -0600

    Add react router link to movie list cards

--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import  './MovieList.css'
 import MovieCard from '../MovieCard/MovieCard'
 
 const MovieList = (props) => {
-  const ListofMovies = props.movies.map(movie => <MovieCard key={movie.id} movieCard={movie} toggleMovie={props.toggleMovie}/>)
+  const ListofMovies = props.movies.map(movie => {
+    return (
+      <Link to={`/${movie.id}`} key={movie.id}>
+        <MovieCard movieCard={movie} />
+      </Link>
+    )
+  })
 
   return (
       <main className="movie-list">
