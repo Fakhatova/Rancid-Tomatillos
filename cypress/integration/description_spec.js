a182b249fb44f72f82ea61500fcd0a8d3c3f2117
Fara Akhatova <faraakhatova@gmail.com>
2021-09-01 15:53:29 -0600

    Add test for rest of the movie details

--- cypress/integration/description_spec.js
+++ cypress/integration/description_spec.js
@@ -1,7 +1,15 @@
+
 describe('Movie Description', () => {
-    it('Should show new page with movie details', () => {
+    it('Should took to a new page with movie details on image click', () => {
+        cy.visit('http://localhost:3000/337401')
+    })
+
+    it('Should see description of the movie', () => {
         cy.visit('http://localhost:3000/337401')
         cy.get('.movie-details')
         cy.get('.movie-image').invoke('attr', 'src').should('not.be.empty')
+        cy.get('.movie-title').contains('Mulan')
+        cy.get('.movie-ratings').contains(5.2727272727272725)
+        cy.get('.movie-release').contains('2020-09-04')
     })
 })
