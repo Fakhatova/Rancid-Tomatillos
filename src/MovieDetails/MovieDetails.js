b456d64417b532d9847822aa02fadf9aa1d81da0
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-01 13:42:21 -0600

    Add conditional rendering for errors

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PageNotFound from './PageNotFound'
 import fetchMovieData from '../API/ApiCalls'
 import '../MovieDetails/MovieDetails.css'
 
@@ -14,18 +15,23 @@ class MovieDetails extends Component {
   componentDidMount = () => {
     fetchMovieData.getSelectedMovie(this.props.movieID)
     .then(data => this.setState({movie: data.movie}))
-    .catch(error => this.setState({error: 'Something went wrong, please try again!'}))
+    .catch(error => this.setState({error: error}))
   }
 
 
   render() {
     return (
-      <article className="movie-details">
-        <img src={this.state.movie.poster_path} alt="Movie Poster" className="movie-image"/>
-        <h4 className="movie-title"> {this.state.movie.title}</h4>
-        <p className="movie-ratings"> {this.state.movie.average_rating}</p>
-        <p className="movie-release"> {this.state.movie.release_date}</p>
-      </article>
+      <>
+        {this.state.error && <PageNotFound/>}
+        {!this.state.error &&
+          <article className="movie-details">
+            <img src={this.state.movie.poster_path} alt="Movie Poster" className="movie-image"/>
+            <h4 className="movie-title"> {this.state.movie.title}</h4>
+            <p className="movie-ratings"> {this.state.movie.average_rating}</p>
+            <p className="movie-release"> {this.state.movie.release_date}</p>
+          </article>
+        }
+      </>
     );
   }
 }
