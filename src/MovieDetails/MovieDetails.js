1c5155c21f91c28d0769258b815b85ae1ecf9ae2
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-01 11:02:29 -0600

    Remove commentedcode

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -10,8 +10,6 @@ class MovieDetails extends Component {
     }
   }
 
-
-
   render() {
     return (
       <article className="movie-details">
@@ -23,18 +21,5 @@ class MovieDetails extends Component {
     );
   }
 }
-
-
-// const MovieDetails = ({movie}) => {
-//   return (
-//     <article className="movie-details">
-//       <img src={movie.poster_path} alt="" className="movie-image"/>
-//        <h4 className="movie-title"> {movie.title}</h4>
-//        <p className="movie-ratings"> {movie.average_rating}</p>
-//        <p className="movie-release"> {movie.release_date}</p>
-//     </article>
-//   )
-// }
-
 
   export default MovieDetails
