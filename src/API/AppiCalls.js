e6122794c4537704005959bc9d79d729f834b470
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-01 11:14:36 -0600

    Change api calls to an object with methods

--- src/API/AppiCalls.js
+++ src/API/AppiCalls.js
@@ -1,6 +1,13 @@
-const fetchMovieData = () => {
-  return  fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response =>response.json())
+const fetchMovieData = {
+  getAllMovies: () => {
+    return  fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response =>response.json())
+  },
+  getSelectedMovie: (id) => {
+    return  fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then(response =>response.json())
+  }
+
 }
 
 export default fetchMovieData
