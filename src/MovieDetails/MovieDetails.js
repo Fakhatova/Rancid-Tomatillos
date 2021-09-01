16efbf833958138e9b01c62eed765956348fc7dd
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-01 11:00:52 -0600

    Change movie details to a class component

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../MovieDetails/MovieDetails.css'
 
-const MovieDetails = ({movie}) => {
-  return (
-    <article className="movie-details">
-      <img src={movie.poster_path} alt="" className="movie-image"/>
-       <h4 className="movie-title"> {movie.title}</h4>
-       <p className="movie-ratings"> {movie.average_rating}</p>
-       <p className="movie-release"> {movie.release_date}</p>
-    </article>
-  )
+class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: null,
+      error: ''
+    }
+  }
+
+
+
+  render() {
+    return (
+      <article className="movie-details">
+        <img src={this.state.movie.poster_path} alt="" className="movie-image"/>
+        <h4 className="movie-title"> {this.state.movie.title}</h4>
+        <p className="movie-ratings"> {this.state.movie.average_rating}</p>
+        <p className="movie-release"> {this.state.movie.release_date}</p>
+      </article>
+    );
+  }
 }
+
+
+// const MovieDetails = ({movie}) => {
+//   return (
+//     <article className="movie-details">
+//       <img src={movie.poster_path} alt="" className="movie-image"/>
+//        <h4 className="movie-title"> {movie.title}</h4>
+//        <p className="movie-ratings"> {movie.average_rating}</p>
+//        <p className="movie-release"> {movie.release_date}</p>
+//     </article>
+//   )
+// }
 
 
   export default MovieDetails
