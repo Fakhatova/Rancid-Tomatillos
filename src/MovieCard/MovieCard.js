1a9a518581aaa3b44cd5feb6dcbd50ce76fa26c0
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-09-02 13:26:02 -0600

    Change card sizing

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ const MovieCard = ({movieCard}) => {
   return (
     <article className="movie-card">
       <img  className="movie-image" src={movieCard.poster_path} alt=""/>
-       <h3 className="movie-title"> {movieCard.title}</h3>
+       <p className="movie-title"> {movieCard.title}</p>
        <p className="movie-ratings"> {movieCard.average_rating}</p>
        <p className="movie-release">{movieCard.release_date}</p>
     </article>
