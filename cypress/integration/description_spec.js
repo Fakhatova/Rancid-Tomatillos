87e7fb15fae55634543b07ed9cdd5336068190ca
Fara Akhatova <faraakhatova@gmail.com>
2021-09-03 10:02:59 -0600

    Fix failing test for movie rating

--- cypress/integration/description_spec.js
+++ cypress/integration/description_spec.js
@@ -9,7 +9,7 @@ describe('Movie Description', () => {
         cy.get('.movie-details')
         cy.get('.movie-image').invoke('attr', 'src').should('not.be.empty')
         cy.get('.movie-title').contains('Mulan')
-        cy.get('.movie-ratings').contains(5.2727272727272725)
+        cy.get('.movie-ratings').contains(5)
         cy.get('.movie-release').contains('2020-09-04')
     })
     it('Should take back to home page if home link is clicked', () => {
