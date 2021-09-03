914fc234cf4b1797137ce9da413171a593348c61
Fara Akhatova <faraakhatova@gmail.com>
2021-09-02 19:08:38 -0600

    add comditio0n to acces fetched data to render genres

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -6,7 +6,8 @@ const MovieCard = ({movieCard}) => {
     <article className="movie-card">
       <img  className="movie-image" src={movieCard.poster_path} alt=""/>
       <div className="movie-info">
-        <p className="movie-title"> {movieCard.title}</p>       <p className="movie-ratings"> Rating: {movieCard.average_rating}</p>
+        <p className="movie-title"> {movieCard.title}</p>       
+        <p className="movie-ratings"> Rating: {movieCard.average_rating}</p>
         <p className="movie-release">Date Released: {movieCard.release_date}</p>
       </div>
     </article>
--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -9,28 +9,35 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       error: ''
+      
     }
     // i dont see us using props anywhere?
   }
 
   componentDidMount = () => {
-    console.log(this.props)
     fetchMovieData.getSelectedMovie(this.props.movieID)
     .then(data => this.setState({movie: data.movie}))
     .catch(error => this.setState({error: error}))
   }
-
+   
+// we need to use as much adata as we can, on movie description. It seems like only date of release and description
+// and title,image rating seems pretty not engaging with user. Not enought content to catch users attention
 
   render() {
-    
-    return (
-      <>
+    let movie;
+    if (this.state.movie.id !== this.props.movieID) {
+    movie = <div>Loading...</div>
+    } else {
+      const roundUpRating = Math.round(this.state.movie.average_rating)
+      const genres = this.state.movie.genres.map(genre => genre)
+      movie = <>
         {this.state.error && <PageNotFound/>}
         {!this.state.error &&
           <article className="movie-details">
             <img src={this.state.movie.poster_path} alt="Movie Poster" className="movie-image"/>
             <h4 className="movie-title"> {this.state.movie.title}</h4>
-            <p className="movie-ratings"> Rating : {this.state.movie.average_rating}</p>
+            <p className="movie-ratings"> Rating : {roundUpRating}</p>
+            <p className="genres"> Genres:&nbsp;{genres} </p>
             <section className="description-container">
               <p className="movie-description">{this.state.movie.overview}</p>
             </section>
@@ -38,6 +45,10 @@ class MovieDetails extends Component {
           </article>
         }
       </>
+    }
+    
+    return (
+      <>{movie}</>
     );
   }
 }
